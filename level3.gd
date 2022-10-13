6659ea7bce8500d75d14e8c0a7a377cee5f1c367
Cristiana <cristiana.phelan@gmail.com>
2022-10-12 20:56:19 -0700

    floppyfish

--- level3.gd
+++ level3.gd
@@ -9,11 +9,4 @@ func _ready():
 	$fish.gravity = 3000
 	$fish.speed = 400
 
-# Called when the node enters the scene tree for the first time.
-
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	pass 
